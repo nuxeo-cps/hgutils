b479cc9b453bb97a820bc453fee2301ce7bd76ae
Georges Racinet on purity.racinet.fr <georges@racinet.fr>
2010-08-07 06:19:59 +0200

    script not needed any more

--- bin/strip_product_reorg.sh
+++
@@ -1,3 +0,0 @@
-# looks for revisions whose parent is -1 and strip them
-# This will typically be the Producs reorg from svn 
-for x in `ls`; do (cd $x; hg heads | grep -B1 "\-1" | grep changeset | sed "s/^changeset[:].*[:]//g"|xargs hg strip) done
